eed775d58a2d72f2c5ad5f79a30b44dff4baf391
gino-225 <george_bm@aol.com>
2018-05-09 15:41:40 +0100

    changed number column in articles table to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509141735) do
+ActiveRecord::Schema.define(version: 20180509143412) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
     t.text "content"
-    t.string "number"
+    t.integer "number"
     t.integer "views"
     t.bigint "category_id"
     t.datetime "created_at", null: false
