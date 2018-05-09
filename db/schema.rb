1c2f93cc1ba723a9d03f93ceb432d5c5ec0b69bb
gino-225 <george_bm@aol.com>
2018-05-09 15:24:57 +0100

    Updated article table to have a string field for the lettering, updatedthe controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508185051) do
+ActiveRecord::Schema.define(version: 20180509141735) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180508185051) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "letter"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
