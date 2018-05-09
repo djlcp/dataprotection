c7169c3556fa7ca17f8880e28948fd73d7639ad3
jamiedodds <jamiedodds13@gmail.com>
2018-05-09 17:12:42 +0100

    styling and layout for cms pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
 
-  # get 'articles/index'
-
-  # root to: 'articles#index'
-  # resources :articles
-  # resources :categories
-  # resources :groups
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
 
   scope module: 'frontend' do
-  	resources :home, only: :index
-  	root to: 'home#index'
+    resources :home, only: :index
+    root to: 'home#index'
   end
+
+  root to: 'articles#index'
+  resources :articles
+  resources :categories
+  resources :groups
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
 
 end
