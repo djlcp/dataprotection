73fe61662bf15eb8f65c9b59eab22f6bb9f354dc
gino-225 <george_bm@aol.com>
2018-05-09 15:52:30 +0100

    added published column to the articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509143412) do
+ActiveRecord::Schema.define(version: 20180509144753) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180509143412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "letter"
+    t.integer "published"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
