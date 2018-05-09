52ce71c95b738e5dd63a9269e0548f1c13b50534
jamiedodds <jamiedodds13@gmail.com>
2018-05-09 11:01:33 +0100

    added frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
-  root to: 'articles#index'
-  resources :articles
-  resources :categories
-  resources :groups
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'articles/index'
+
+  # root to: 'articles#index'
+  # resources :articles
+  # resources :categories
+  # resources :groups
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: 'frontend' do
+  	resources :home, only: :index
+  	root to: 'home#index'
+  end
+
 end
