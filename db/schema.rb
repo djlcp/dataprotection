7653925fd472ab5cfe6477713095c91dcfb7d166
gino-225 <george_bm@aol.com>
2018-05-12 15:33:03 +0100

    extra form field from bill that defines if an articles is an article, recital or guidance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510171354) do
+ActiveRecord::Schema.define(version: 20180510193700) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180510171354) do
     t.datetime "updated_at", null: false
     t.string "letter"
     t.boolean "published", default: false
+    t.integer "article_type"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
