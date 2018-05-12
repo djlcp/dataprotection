ff87cff0114f420936ff580c798aef7c734b9eb8
jamiedodds <jamiedodds13@gmail.com>
2018-05-12 14:42:31 +0100

    more styling done for show and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :invitations => 'admin/invitations' }
 
-  # root to: 'articles#index'
+  root to: 'articles#index'
   
   resources :articles, only: [:index, :show]
 
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
     resources :groups
     resources :users 
   end
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
