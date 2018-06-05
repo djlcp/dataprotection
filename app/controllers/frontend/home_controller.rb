104130f976de469f8b2bc3034987364d40d1ff29
StudentIF <hfritz@aol.com>
2018-06-05 19:04:48 +0100

    pull groups from db

--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -7,11 +7,15 @@ class Frontend::HomeController < FrontendController
 
   def regulations_dp
   	@articles = []
-  	categories=Category.where(group_id = '1')
+  	categories=Category.where('group_id = 1')
   	categories.each do |category|
   		articles=Article.where(category_id: category.id)
   		@articles += articles
   	end
+
+    # @group = Group.find(params['1'])
+    # @group = Group.where('id == ?', '1')
+    @groups = Group.all
   end
 
   def dplaws
