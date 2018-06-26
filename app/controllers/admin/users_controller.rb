8812001eaa0583323460df3ad2d2dde3ffeebfed
Daniel Burridge <daniel.burridge@pm.me>
2018-06-26 18:52:28 +0100

    various bug fixes, added title, changed invitations redirect

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
 
   def index
@@ -8,28 +9,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-  end
- 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:notice] = 'New user created'
-      redirect_to admin_user_path
-    else
-      render 'new'
-    end    
-  end
-
-  def new
-    @user = User.new
   end
 
   def update
-    @user = User.find(params[:id])
-
-      if @user.update(user_params)
+    if @user.update(user_params)
         flash[:notice] = 'User Updated'
         redirect_to admin_users_path
     else
@@ -38,21 +21,22 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-      @user = User.find(params[:id])
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
-
     flash[:notice] = "User Removed"
-
     redirect_to admin_users_path
   end
 
   private
 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:email, :first_name, :last_name, :manager)
     end
+
 end
